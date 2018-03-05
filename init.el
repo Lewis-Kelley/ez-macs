8571f6634f9a188986dfa9989f2cece3ddd1a3ba
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-03-05 10:21:16 -0500

    Removed geiser from the init.el.

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rainbow-delimiters paredit hydra magit geiser use-package))))
+    (rainbow-delimiters paredit hydra magit use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
